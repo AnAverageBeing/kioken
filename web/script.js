697d4be2731d84bcdeb038dcade86024818c6bdc
AnAverageBeing <averagebeing@proton.me>
2023-04-15 02:03:44 +0530

    lol

--- web/script.js
+++ web/script.js
@@ -18,7 +18,7 @@ const chart = new Chart(chartCtx, {
                 data: [],
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 2,
+                borderWidth: 4,
                 fill: true,
                 pointRadius: 0,
                 pointHoverRadius: 1
@@ -28,7 +28,7 @@ const chart = new Chart(chartCtx, {
                 data: [],
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 2,
+                borderWidth: 4,
                 fill: true,
                 pointRadius: 0,
                 pointHoverRadius: 1
@@ -38,7 +38,7 @@ const chart = new Chart(chartCtx, {
                 data: [],
                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
                 borderColor: 'rgba(255, 206, 86, 1)',
-                borderWidth: 2,
+                borderWidth: 4,
                 fill: true,
                 pointRadius: 0,
                 pointHoverRadius: 1
