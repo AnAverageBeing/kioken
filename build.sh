681e7adae146651d8685d14fc8310cbcad1d1c9f
AnAverageBeing <averagebeing@proton.me>
2023-04-23 15:41:58 +0530

    wow

--- build.sh
+++ build.sh
@@ -8,8 +8,16 @@ if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir "$OUTPUT_DIR"
 fi
 
-# Build kioken binary
-go build -ldflags="-s -w" -tags netgo -ldflags="-extldflags=-static" -o "$OUTPUT_DIR/kioken" ./cmd/kioken
+# Build kioken binary and capture output and error streams
+BUILD_OUTPUT=$(go build -ldflags="-s -w" -tags netgo -ldflags="-extldflags=-static" -o "$OUTPUT_DIR/kioken" ./cmd/kioken 2>&1)
 
-# Print success message
-echo "Build successful! Binary can be found in $OUTPUT_DIR/kioken"
+# Check if there were any errors during build
+if [ $? -ne 0 ]; then
+  # Print error message in red color and exit script
+  echo -e "\033[31mBuild failed with following error:\033[0m"
+  echo -e "$BUILD_OUTPUT"
+  exit 1
+else
+  # Print success message in green color
+  echo -e "\033[32mBuild successful! Binary can be found in $OUTPUT_DIR/kioken\033[0m"
+fi
